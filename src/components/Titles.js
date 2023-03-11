984dfbe8f7c932e01b3599feb5f5991fa52ec0c0
Matthew <mattameloko12@gmail.com>
2023-03-11 17:10:40 +0100

    updated title

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -4,7 +4,7 @@ class Titles extends Component {
     render() {
         return (
             <div>
-                <h1 className="title-container__title">Weather Finder</h1>
+                <h1 className="title-container__title">Weather Forecast</h1>
                 <h3 className="title-container__subtitle">Find out temperature conditons and more..</h3>                
             </div>
         );
